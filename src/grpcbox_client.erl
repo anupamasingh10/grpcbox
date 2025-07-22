0509837f1d077208c83f6513a9252331098a1000
Anupama Singh <31176057+anupamasingh10@users.noreply.github.com>
2025-07-22 16:27:49 +0200

    Revert "Kill teh stream processs to prevent leaking processes"

--- src/grpcbox_client.erl
+++ src/grpcbox_client.erl
@@ -225,13 +225,6 @@ recv(Type, #{stream_id := Id,
                     {error, unknown}
             end
     after Timeout ->
-            %% Kill the stream process to prevent resource leaks
-            case erlang:is_process_alive(Pid) of
-                true ->
-                    h2_stream:stop(Pid);
-                false ->
-                    ok
-            end,
             {error, timeout}
     end.
 
@@ -258,13 +251,6 @@ recv_end(#{stream_id := StreamId,
         {'DOWN', Ref, process, Pid, Reason} ->
             {error, {stream_down, Reason}}
     after Timeout ->
-            %% Kill the stream process to prevent resource leaks
-            case erlang:is_process_alive(Pid) of
-                true ->
-                    h2_stream:stop(Pid);
-                false ->
-                    ok
-            end,
             {error, timeout}
     end.
 
